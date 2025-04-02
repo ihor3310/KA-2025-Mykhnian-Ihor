9a5b109a75288e63e21cb3b1e36a301930fb811d
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2025-04-02 18:30:13 +0300

    Added logic for finding the median (command line)
    created a function for finding the median and passing arguments from the command line (conversion to binary representation has not yet been applied)

--- Project/Project.py
+++ Project/Project.py
@@ -1,19 +1,26 @@
 import sys
 
-def read_input_file(filename):
+def binary_convert():
     pass
 
-def mediana():
+def mediana(data):
+    data_int = list(map(int, data.split()))
+    len_arr = len(data_int)
+
+    if len_arr % 2 == 0:
+        return (data_int[len_arr // 2 - 1] + data_int[len_arr // 2]) / 2
+    else:
+        return data_int[len_arr // 2]
+
+def average_value(data):
     pass
 
-def average_value():
-    pass
-
-input_data = read_input_file(filename)
-print(f"Filename: {filename}")  
-print(input_data)
+def analz_file(filename):
+    with open(filename, "r") as data:
+        return data.read()
 
 if __name__ == "__main__":
-    med = mediana()
-    mid_ar = average_value()
-    print(med, "\n", mid_ar)
+    filename = sys.argv[1]
+    file_data = analz_file(filename)
+    med = mediana(file_data)
+    print(med)
