c4952a7364f2b2515aab03cf6dd602bde5675d6e
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2025-04-02 14:57:07 +0300

    Added code structure

--- Project/Project.py
+++ Project/Project.py
@@ -1,3 +1,18 @@
+import sys
+
+def read_input_file(filename):
+    pass
+
+def mediana():
+    pass
+
+def average_value():
+    pass
+
+input_data = read_input_file(filename)
+print(f"Filename: {filename}")  
+print(input_data)
+
 if __name__ == "__main__":
     med = mediana()
     mid_ar = average_value()
