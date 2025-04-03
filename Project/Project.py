7e6165f5fcb83c6dde3402f89de014b000c23c99
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2025-04-03 19:58:12 +0200

    Added sorting algorithm
    sorting algorithm "bubble sort" will be added later "merge sort"
     (conversion to binary representation has not yet been applied)

--- Project/Project.py
+++ Project/Project.py
@@ -1,20 +1,27 @@
 import sys
 
-def binary_convert():
-    pass
-    
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] 
+    return arr
+
 def mediana(data):
     data_int = list(map(int, data.split()))
-    len_arr = len(data_int)
+    sorted_data = bubble_sort(data_int) 
+    len_arr = len(sorted_data)
 
     if len_arr % 2 == 0:
-        return (data_int[len_arr // 2 - 1] + data_int[len_arr // 2]) / 2
+        return (sorted_data[len_arr // 2 - 1] + sorted_data[len_arr // 2]) / 2
     else:
-        return data_int[len_arr // 2]
+        return sorted_data[len_arr // 2]
 
 def average_value(data):
     data_int = list(map(int, data.split()))
-    return sum(data_int) // len(data_int)
+    sorted_data = bubble_sort(data_int)
+    return sum(sorted_data) // len(sorted_data)
 
 def analz_file(filename):
     with open(filename, "r") as data:
