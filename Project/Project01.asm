a920580a0f3db5e6eff918666ced3e97f3c293ae
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2025-04-10 20:54:51 +0300

    Reading data from the command line
    separately make reading arguments from the command line then combine these codes

--- Project/Project01.asm
+++ Project/Project01.asm
@@ -1,28 +1,41 @@
 .model small
 .stack 100h
+
 .data
+    newline db 0, '$'
+
 .code
-start:
+main proc
     mov ax, @data
     mov ds, ax
-    mov ax, 413
-    mov bx, ax
-    mov cx, 16
-
-print_bits:
-    rol bx, 1
-    jc swap1
-    mov dl, '0'
-    jmp done
-
-swap1:
-    mov dl, '1'
-
-done:
+    
+    mov bx, 80h
+    xor cx, cx             
+    mov cl, byte ptr es:[bx]
+    
+    cmp cx, 0
+    je exit
+    
+    inc bx
+output_loop:
+    cmp cx, 0
+    je output_done
+    
+    mov dl, byte ptr es:[bx]
     mov ah, 02h
     int 21h
-    loop print_bits
-
-    mov ah, 4Ch
+    
+    inc bx
+    dec cx
+    jmp output_loop
+    
+output_done:
+    lea dx, newline
+    mov ah, 09h
     int 21h
-end start
+    
+exit:
+    mov ah, 4ch
+    int 21h
+main endp
+end main
