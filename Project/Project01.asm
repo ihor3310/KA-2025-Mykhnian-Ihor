35e1055c8685b25865c81018753bc54e3df961c7
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2025-04-11 01:04:52 +0300

    Converts 16-bit values
    previous errors fixed, now conversion occurs for each number in the file

--- Project/Project01.asm
+++ Project/Project01.asm
@@ -4,6 +4,7 @@
     newline db 10 dup(0)
     buffer db 255 dup(0), '$'
     save_id dw 0
+    tog_id db 0, 10, '$'
 .code
 main proc
     mov ax, @data
@@ -11,7 +12,9 @@ main proc
     mov bx, 80h
     mov cl, byte ptr es:[bx]
     cmp cx, 0
-    je exit
+    jne cont_main
+    jmp exit
+cont_main:
     xor si, si
     inc bx
 skip:
@@ -32,7 +35,9 @@ open:
     mov ax, 3D00h
     lea dx, newline
     int 21h
-    jc exit
+    jnc cont_open
+    jmp exit
+cont_open:
     mov save_id, ax
 read:
     mov ah, 3Fh
@@ -49,35 +54,75 @@ read:
     int 21h
     jmp read
 end_r:
-    lea si, buffer  
-process_numbers:
-    mov al, [si]   
-    cmp al, '$'   
-    je exit       
-    cmp al, ' '     
-    je skip_space 
-    sub al, '0'  
-    cbw            
-    mov bx, ax      
-    mov cx, 16
-
-print_bin:
-    shl bx, 1   
-    jc print_1
-    mov dl, '0'
-    jmp show_bit
-print_1:
-    mov dl, '1'
-show_bit:
-    mov ah, 02h
-    int 21h
-    loop print_bin
-    
-skip_space:
-    inc si
-    jmp process_numbers
-
+    lea si, buffer
+proc_numbers:
+    skip_spaces:
+        mov al, [si]
+        cmp al, '$'
+        je exit_from_process
+        cmp al, 0Dh
+        je exit_from_process
+        cmp al, 0Ah
+        je exit_from_process
+        cmp al, ' '
+        jne start_number
+        inc si
+        jmp skip_spaces
+    start_number:
+        cmp al, '0'
+        jb skip_non_digit
+        cmp al, '9'
+        ja skip_non_digit
+        mov bx, 0
+    parse_number:
+        mov al, [si]
+        cmp al, '0'
+        jb end_of_number
+        cmp al, '9'
+        ja end_of_number
+        mov cx, 10
+        mov ax, bx
+        mul cx
+        mov bx, ax
+        mov al, [si]
+        sub al, '0'
+        cbw
+        add bx, ax
+        inc si
+        jmp parse_number
+    end_of_number:
+        push si
+        mov ah, 9
+        lea dx, tog_id
+        int 21h
+        pop si
+        mov cx, 16
+    print_bin:
+        shl bx, 1
+        jc print_1
+        mov dl, '0'
+        jmp show_bit
+    print_1:
+        mov dl, '1'
+    show_bit:
+        mov ah, 02h
+        int 21h
+        loop print_bin
+        push si
+        mov ah, 9
+        lea dx, tog_id
+        int 21h
+        pop si
+        jmp proc_numbers
+    skip_non_digit:
+        inc si
+        jmp proc_numbers
+exit_from_process:
+    jmp exit
 exit:
+    mov ah, 3Eh
+    mov bx, save_id
+    int 21h
     mov ah, 4Ch
     int 21h
 main endp
